3ab0ae056817cecbc9ec0af7101e969d5e03bbb5
rkb94 <sfo740s.john@gmail.com>
2018-03-21 22:34:25 +0900

    node.js로 웹 애플리케이션 만들기

--- app_file.js
+++ app_file.js
@@ -1,13 +1,47 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
 app.locals.pretty = true;
 app.set('views', './views_file'); // 템플릿 엔진의 파일들은 ./views_file 안에 두겠다고 express에 설정을 해둔다.
 app.set('view engine', 'jade'); // 어떠한 템플릿 엔진을 쓸 것인지에 대해 view engine이라고 하는 값으로 jade를 쓸것을 적는다. 이 때 jade를 설치 해야한다.
 app.get('/topic/new', function(req, res){
     res.render('new');
-})
+});
+app.get('/topic', function(req, res){
+    fs.readdir('data', function(err, files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internet Server Error');
+        }
+        res.render('view', {topics:files});
+    })
+});
+app.get('/topic/:id', function(req, res){
+    var id = req.params.id;
+    fs.readdir('data', function(err, files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internet Server Error');
+        }
+        fs.readFile('data/'+id, 'utf8', function(err, data){
+            if(err){
+                res.status(500).send('Internet Server Error');
+            }
+            res.render('view', {topics:files, title:id, description:data});
+        })
+    })
+});
 app.post('/topic', function(req, res){
-    res.send('Hi, post');
+    var title = req.body.title;
+    var description = req.body.description;
+    fs.writeFile('data/'+title, description, function(err){
+        if(err){
+            res.status(500).send('Internet Server Error');
+        }
+        res.render('view', {title:id});
+    });
 })
 app.listen(3000, function(){
     console.log('Connected, 3000 port!');
